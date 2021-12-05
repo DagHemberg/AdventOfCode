99e8b595dca249c799239057039d06178af183e0
DagHemberg <71427286+DagHemberg@users.noreply.github.com>
2021-12-05 15:34:09 +0100

    problem 2

--- 5/src/main/scala/Main.scala
+++ 5/src/main/scala/Main.scala
@@ -3,14 +3,20 @@ package aoc.day05
 case class Pos(x: Int, y: Int)
 
 case class Line(start: Pos, end: Pos):
+
+  val xRange = (start.x min end.x) to (start.x max end.x)
+  val yRange = (start.y min end.y) to (start.y max end.y)
+
+  def isDiagonal = (start.x - end.x).abs == (start.y - end.y).abs
+
   def inside =
-    if start.x == end.x then
-      ((start.y min end.y) to (start.y max end.y)).map(y => Pos(start.x, y)).toVector
-    else if start.y == end.y then
-      ((start.x min end.x) to (start.x max end.x)).map(x => Pos(x, start.y)).toVector
-    else
-      // todo?
-      Vector.empty[Pos]
+    if      start.x == end.x then yRange.map(y => Pos(start.x, y)).toVector
+    else if start.y == end.y then xRange.map(x => Pos(x, start.y)).toVector
+    else if isDiagonal then
+      val xs = if start.x < end.x then xRange else xRange.reverse
+      val ys = if start.y < end.y then yRange else yRange.reverse
+      xs.zip(ys).map(Pos.apply)
+    else Vector.empty[Pos]
 
 @main
 def run =
@@ -22,6 +28,19 @@ def run =
 
   val allPositions = lines.flatMap(_.inside)
   val intersections = allPositions.groupBy(identity).filter(_._2.size > 1).keys.toVector
+  
+  // for debugging purposes
+  // val field = Vector
+  //   .tabulate(allPositions.maxBy(_.y).y + 1, allPositions.maxBy(_.x).x + 1)(
+  //     (y, x) =>
+  //       if allPositions.contains(Pos(x, y)) then
+  //         allPositions.count(_ == Pos(x, y))
+  //       else "."
+  //   )
+  //   .map(_.mkString)
+  //   .mkString("\n")
+
+  // println(field)
 
   println(allPositions.size)
   println(intersections.size)
