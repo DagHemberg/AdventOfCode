8d950f52503c9ad5a8f7baf7a70266a0d051cbf9
DagHemberg <71427286+DagHemberg@users.noreply.github.com>
2021-12-06 14:20:48 +0100

    fixed input path

--- 2021/6/scl/Main.scala
+++ 2021/6/scl/Main.scala
@@ -1,5 +1,5 @@
 @main def run = 
-    val input = scala.io.Source.fromFile("input.txt").getLines.flatMap(_.split(",")).map(_.toInt).toVector
-    val fishes: Vector[Long] = (0 to 8).map(i => input.count(_ == i)).toVector.map(_.toLong)
+    val input = scala.io.Source.fromFile("../input.txt").getLines.flatMap(_.split(",")).map(_.toInt).toVector
+    val fishes = (0 to 8).map(i => input.count(_ == i)).toVector.map(_.toLong)
     val result = (0 until 256).foldLeft(fishes)((c, _) => c.tail.updated(6, c.tail(6) + c.head) :+ c.head).sum
     println(result)
