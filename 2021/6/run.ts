4653c4b10bc0e5ff837efed8576424f3f655b5c9
DagHemberg <71427286+DagHemberg@users.noreply.github.com>
2021-12-06 10:11:37 +0100

    day 6 problem 1

---
+++ 2021/6/run.ts
@@ -0,0 +1,20 @@
+import * as fs from 'fs'
+
+class fish {
+    daysUntilNew: number
+    
+    constructor(n: number) {
+        this.daysUntilNew = n
+    }
+
+    nextDay() {
+        return this.daysUntilNew - 1 < 0 ? [new fish(6), new fish(8)] : [new fish(this.daysUntilNew - 1)]
+    }
+}
+
+const raw = fs.readFileSync("input.txt", "utf-8")
+const fishes: fish[] = raw.split(",").map(x => parseInt(x)).map(age => new fish(age))
+
+const res = [...Array(256).keys()].reduce((data, _) => data.flatMap(x => x.nextDay()), fishes)
+
+console.log(res.length)
