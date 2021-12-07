989c19e75e5755620b032a949c6ad29cea5bf481
DagHemberg <71427286+DagHemberg@users.noreply.github.com>
2021-12-07 08:14:52 +0100

    refactoring

--- 2021/6/run.ts
+++ 2021/6/run.ts
@@ -1,5 +1,4 @@
 import * as fs from 'fs'
-
 const raw = fs.readFileSync('input.txt', 'utf8')
 
 // fishes grouped by days left
@@ -7,10 +6,8 @@ var fishes: number[] = [...Array(9).keys()]
     .map(fishGroup => raw.split(",").map(Number).filter(fish => fish == fishGroup).length) 
 
 for (let i = 0; i < 256; i++) {
-    fishes.push(0)
-    const zeroes = fishes.shift() as number
-    fishes[6] += zeroes
-    fishes[8] += zeroes
+    fishes.push(fishes.shift() as number)
+    fishes[6] += fishes[8]
 }
 
 console.log(fishes.reduce((acc, curr) => acc + curr, 0))
