2322f267ccdb5276123c8c8e54bba6f6c9e9a45d
DagHemberg <71427286+DagHemberg@users.noreply.github.com>
2021-12-09 14:59:08 +0100

    problemo dos

---
+++ 2021/scala/src/main/scala/aoc/day09/Problem2.scala
@@ -0,0 +1,32 @@
+package aoc.day09
+
+import aoc.utils.*
+
+object Problem2 extends Solver("09", 1134):
+  def solve(data: Vector[String]) = 
+    val matrix = Matrix(data.map(_.split("").map(_.toInt).toVector))
+
+    val lowPoints = matrix.indices
+      .map(i => (matrix.surround(i.row, i.col), i))
+      .filter((surrounding, index) =>
+        surrounding.forall(matrix(index) < matrix(_))
+      )
+      .map((_, pos) => pos) 
+
+    val moats = lowPoints.map(pos =>
+      val collected = scala.collection.mutable.Set(pos)
+      var prevSize = 0
+      while collected.size != prevSize do
+        prevSize = collected.size
+        collected.foreach(ind =>
+          matrix
+            .surround(ind.row, ind.col)
+            .toSet
+            .diff(collected)
+            .foreach(index => if matrix(index) != 9 then collected += index)
+        )
+
+      collected.size
+    )
+
+    moats.sorted.reverse.take(3).product
