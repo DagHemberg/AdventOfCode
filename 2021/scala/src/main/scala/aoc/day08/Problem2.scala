d668972a5a99cad52b7d57d9e4a619207402c14e
DagHemberg <71427286+DagHemberg@users.noreply.github.com>
2021-12-09 02:28:08 +0100

    remove unnecessary .toSet

--- 2021/scala/src/main/scala/aoc/day08/Problem2.scala
+++ 2021/scala/src/main/scala/aoc/day08/Problem2.scala
@@ -6,7 +6,7 @@ object Problem2 extends Solver("08", 61229):
   def solve(data: Vector[String]) =
 
     val parsed = data.map { case s"$signalPatterns | $output" =>
-      signalPatterns.split(" ").toVector -> output.split(" ").toVector
+      signalPatterns.split(" ").map(_.toSet).toVector -> output.split(" ").map(_.toSet).toVector
     }.toVector
 
     val decryptedNumbers = parsed.map((signalPatterns, output) =>
@@ -16,11 +16,11 @@ object Problem2 extends Solver("08", 61229):
       val sixes = signalPatterns.filter(_.size == 6).map(_.toSet)
 
       // my linter is crying but i am not giving in
-      letters.addOne(1 -> signalPatterns.find(_.size == 2).get.toSet)
-      letters.addOne(4 -> signalPatterns.find(_.size == 4).get.toSet)
-      letters.addOne(7 -> signalPatterns.find(_.size == 3).get.toSet)
-      letters.addOne(8 -> signalPatterns.find(_.size == 7).get.toSet)
-      letters.addOne(3 -> signalPatterns.find(_.toSet.diff(letters(1)).size == 3).get.toSet)
+      letters.addOne(1 -> signalPatterns.find(_.size == 2).get)
+      letters.addOne(4 -> signalPatterns.find(_.size == 4).get)
+      letters.addOne(7 -> signalPatterns.find(_.size == 3).get)
+      letters.addOne(8 -> signalPatterns.find(_.size == 7).get)
+      letters.addOne(3 -> signalPatterns.find(_.diff(letters(1)).size == 3).get)
       letters.addOne(2 -> fives.filter(_.intersect(letters(4)).size == 2).head)
       letters.addOne(5 -> fives.filter(_ != letters(3)).filter(_.intersect(letters(4)).size == 3).head)
       letters.addOne(9 -> sixes.filter(_.intersect(letters(4)).size == 4).head)
@@ -28,7 +28,7 @@ object Problem2 extends Solver("08", 61229):
       letters.addOne(0 -> sixes.filter(_ != letters(6)).filter(_ != letters(9)).head)
 
       output
-        .flatMap(out => letters.filter(_._2 == out.toSet).map(_._1).toVector)
+        .flatMap(out => letters.filter(_._2 == out).map(_._1).toVector)
         .mkString
         .toInt
     )
