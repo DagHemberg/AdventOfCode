47bcd21541989ab9bc392241dd04fea5a3fd7a77
DagHemberg <71427286+DagHemberg@users.noreply.github.com>
2021-12-09 14:58:44 +0100

    added Index class

--- 2021/scala/src/main/scala/aoc/day09/Matrix.scala
+++ 2021/scala/src/main/scala/aoc/day09/Matrix.scala
@@ -1,10 +1,12 @@
 package aoc.day09
 
+case class Index(row: Int, col: Int)
+
 case class Matrix(input: Vector[Vector[Int]]):
   override def toString = input.map(_.mkString(" ")).mkString("\n")
 
   def apply(row: Int, col: Int): Int = input(row)(col)
-  def apply(coords: (Int, Int)): Int = input(coords._1)(coords._2)
+  def apply(index: Index): Int = input(index.row)(index.col)
 
   private def indexOutside(row: Int, col: Int) =
     row < 0 || row >= height || col < 0 || col >= width
@@ -19,13 +21,13 @@ case class Matrix(input: Vector[Vector[Int]]):
     input.slice(row, row + width).map(_.slice(col, col + height))
   )
 
-  def indices = (0 until height).flatMap(row => (0 until width).map(col => (row, col))).toVector
+  def indices = (0 until height).flatMap(row => (0 until width).map(col => Index(row, col))).toVector
 
   def map(f: Int => Int): Matrix = Matrix(input.map(_.map(f)))
 
   def surround(row: Int, col: Int) = 
     val (up, down, left, right) = ((row - 1, col), (row + 1, col), (row, col - 1), (row, col + 1))
-    Vector(up, down, left, right).filter(!indexOutside(_, _))
+    Vector(up, down, left, right).filter(!indexOutside(_, _)).map(Index.apply)
 
   // def surround(row: Int, col: Int): Matrix =
   //   slice(row - 1, col - 1)(3, 3)
