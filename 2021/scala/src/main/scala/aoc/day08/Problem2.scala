bf68f074674337bd796143f0d1372f4b4bcf5ffd
DagHemberg <71427286+DagHemberg@users.noreply.github.com>
2021-12-09 02:21:11 +0100

    compacct

--- 2021/scala/src/main/scala/aoc/day08/Problem2.scala
+++ 2021/scala/src/main/scala/aoc/day08/Problem2.scala
@@ -9,64 +9,28 @@ object Problem2 extends Solver("08", 61229):
       signalPatterns.split(" ").toVector -> output.split(" ").toVector
     }.toVector
 
-    val decrypted = parsed.map((signalPatterns, output) =>
+    val decryptedNumbers = parsed.map((signalPatterns, output) =>
 
       val letters = scala.collection.mutable.Map[Int, Set[Char]]()
       val fives = signalPatterns.filter(_.size == 5).map(_.toSet)
       val sixes = signalPatterns.filter(_.size == 6).map(_.toSet)
 
-      // the easy ones
+      // my linter is crying but i am not giving in
       letters.addOne(1 -> signalPatterns.find(_.size == 2).get.toSet)
       letters.addOne(4 -> signalPatterns.find(_.size == 4).get.toSet)
       letters.addOne(7 -> signalPatterns.find(_.size == 3).get.toSet)
       letters.addOne(8 -> signalPatterns.find(_.size == 7).get.toSet)
-
-      letters.addOne(
-        3 -> signalPatterns
-          .find(_.toSet.diff(letters(1)).size == 3)
-          .get
-          .toSet
-      )
-
-      // the hard ones
-      letters.addOne(
-        2 -> fives
-          .filter(_.intersect(letters(4)).size == 2)
-          .head
-      )
-
-      letters.addOne(
-        5 -> fives
-          .filter(_ != letters(3))
-          .filter(_.intersect(letters(4)).size == 3)
-          .head
-      )
-
-      letters.addOne(
-        9 -> sixes
-          .filter(_.intersect(letters(4)).size == 4)
-          .head
-      )
-
-      letters.addOne(
-        6 -> sixes
-          .find(_.union(letters(1)).size == 7)
-          .get
-      )
-
-      letters.addOne(
-        0 -> sixes
-          .filter(_ != letters(6))
-          .filter(_ != letters(9))
-          .head
-      )
+      letters.addOne(3 -> signalPatterns.find(_.toSet.diff(letters(1)).size == 3).get.toSet)
+      letters.addOne(2 -> fives.filter(_.intersect(letters(4)).size == 2).head)
+      letters.addOne(5 -> fives.filter(_ != letters(3)).filter(_.intersect(letters(4)).size == 3).head)
+      letters.addOne(9 -> sixes.filter(_.intersect(letters(4)).size == 4).head)
+      letters.addOne(6 -> sixes.find(_.union(letters(1)).size == 7).get)
+      letters.addOne(0 -> sixes.filter(_ != letters(6)).filter(_ != letters(9)).head)
 
       output
-        .flatMap(out =>
-          letters.filter(_._2 == out.toSet).map(_._1).toVector
-        ) // getting the key from the lock
+        .flatMap(out => letters.filter(_._2 == out.toSet).map(_._1).toVector)
         .mkString
         .toInt
     )
 
-    decrypted.sum
+    decryptedNumbers.sum
