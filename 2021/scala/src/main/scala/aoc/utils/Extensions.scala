a68950bdfa30db126a40469d75b80f3fd41976cd
DagHemberg <71427286+DagHemberg@users.noreply.github.com>
2021-12-11 11:58:34 +0100

    Added useful common elements for all problems

---
+++ 2021/scala/src/main/scala/aoc/utils/Extensions.scala
@@ -0,0 +1,6 @@
+package aoc.utils
+
+extension [A](a: A)
+  def debug = 
+    println(a)
+    a
---
+++ 2021/scala/src/main/scala/aoc/utils/usefulClasses.scala
@@ -0,0 +1,4 @@
+package aoc.utils
+
+case class Pos(x: Int, y: Int)
+case class Line(start: Pos, end: Pos)
