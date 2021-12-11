ba12a22c1e2e963d55c20fbb0e864268941f78d1
DagHemberg <71427286+DagHemberg@users.noreply.github.com>
2021-12-11 12:01:28 +0100

    moved surrounding function from matrix

--- 2021/scala/src/main/scala/aoc/day09-smoke-basin/Problem1.scala
+++ 2021/scala/src/main/scala/aoc/day09-smoke-basin/Problem1.scala
@@ -1,6 +1,11 @@
 package aoc.day09
 import aoc.utils.*
 
+extension (m: Matrix[Int])
+  def surrounding(row: Int, col: Int) = 
+    val (up, down, left, right) = ((row - 1, col), (row + 1, col), (row, col - 1), (row, col + 1))
+    Vector(up, down, left, right).filter(!m.indexOutsideBounds(_, _)).map(Index.apply)
+
 object Problem1 extends Solver("09", 15):
   def name = "Smoke Basin - Part 1"
   def solve(data: Vector[String]) =
