3ca95f09f2f5be6e035c230f023de611d15272d9
DagHemberg <71427286+DagHemberg@users.noreply.github.com>
2021-12-11 19:05:21 +0100

    added some (probably not very useful) methods for numeric vectors

--- 2021/scala/src/main/scala/aoc/utils/Extensions.scala
+++ 2021/scala/src/main/scala/aoc/utils/Extensions.scala
@@ -1,6 +1,26 @@
 package aoc.utils
 
+import math.Numeric.Implicits.infixNumericOps
+
 extension [A](a: A)
   def debug = 
     println(a)
     a
+
+// will these be useful?
+// probably not but they're fun to do
+extension [A: Numeric](xs: Vector[A])
+  infix def dot (ys: Vector[A]) = 
+    require(xs.size == ys.size, "Vectors must be the same size")
+    xs.zip(ys).map((x, y) => x * y).sum
+
+  infix def cross (ys: Vector[A]) = 
+    require(xs.size == 3 && ys.size == 3, "Cross product only defined for 3D vectors")
+    Vector(
+      xs(1) * ys(2) - xs(2) * ys(1), 
+      xs(2) * ys(0) - xs(0) * ys(2), 
+      xs(0) * ys(1) - xs(1) * ys(0)
+    )
+
+  def len = math.sqrt((xs dot xs).toDouble)
+  def normalized = xs.map(_.toDouble * (1.0 / xs.len))
