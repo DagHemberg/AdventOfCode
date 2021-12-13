e3a396d7109b19e8b44803176853f7b198e01a85
DagHemberg <71427286+DagHemberg@users.noreply.github.com>
2021-12-13 18:30:00 +0100

    fixed mega inefficient solution

--- 2021/scala/src/main/scala/aoc/day13-transparent-origami/Problem2.scala
+++ 2021/scala/src/main/scala/aoc/day13-transparent-origami/Problem2.scala
@@ -8,50 +8,28 @@ object Problem2 extends Solver("13", "See above"):
       .takeWhile(_.nonEmpty)
       .map { case s"$x,$y" => Pos(x.toInt, y.toInt) }
 
-    val commands = data.reverse
+    val commands = data
+      .reverse
       .takeWhile(_.nonEmpty)
       .reverse
       .map { case s"fold along $dir=$value" => Command(dir, value.toInt) }
 
-    val initPaper =
-      Matrix(points.maxBy(_.y).y + 1, points.maxBy(_.x).x + 1)((x, y) =>
-        points.find(_ == Pos(y, x)).isDefined
-      )
-
-    val newPaper = commands.foldLeft(initPaper)((paper, command) => command match
-      case Command("y", n) =>
-        val top = paper.slice(0, 0)(n, paper.width)
-        val bottom = paper.slice(n + 1, 0)(paper.height, paper.width).flipRows
-        if top.height == bottom.height then
-          top zip bottom map ((top, btm) => top || btm)
-        else
-          val shorter = if top.height < bottom.height then top else bottom
-          val taller = if top.height > bottom.height then top else bottom
-          val lessShort = shorter.appendedTop(Matrix
-            (taller.height - shorter.height, shorter.width)
-            ((row, col) => false)
-          )
-          lessShort zip taller map ((short, tall) => short || tall)
-      case Command("x", n) =>
-        val left = paper.slice(0, 0)(paper.height, n)
-        val right = paper.slice(0, n + 1)(paper.height, paper.width).flipCols
-        if left.width == right.width then
-          left zip right map ((left, right) => left || right)
-        else
-          val thinner = if left.width < right.width then left else right
-          val wider = if left.width > right.width then left else right
-          val lessThin = thinner.appendedLeft(
-            Matrix(thinner.height, wider.width - thinner.width)((row, col) =>
-              false
-            )
-          )
-          lessThin zip wider map ((thin, wide) => thin || wide)
+    val folded = commands.foldLeft(points.toSet)((pts, command) => command match
+      case Command("x", n) => pts.map(p => if p.x < n then p else Pos(2 * n - p.x, p.y))
+      case Command("y", n) => pts.map(p => if p.y < n then p else Pos(p.x, 2 * n - p.y))
     )
 
-    val text = newPaper.toVector.map(_.map(if _ then s"${Console.GREEN_B} ${Console.RESET}" else " ").mkString("│ ", "", " │")).mkString("\n")
+    val textWidth = folded.maxBy(_.x).x + 1
+    val text = Matrix
+      (folded.maxBy(_.y).y + 1, folded.maxBy(_.x).x + 1)
+      ((x, y) => folded(Pos(y, x)))
+        .toVector
+        .map(_.map(if _ then s"${Console.GREEN_B}  ${Console.RESET}" else "  "))
+        .map(r => s"    │ ${r.mkString} │")
+        .mkString("\n")
 
-    println(s"""|\n┌─${Vector.fill(newPaper.width)("─").mkString}─┐
+    println(s"""|[${Console.YELLOW}!${Console.RESET}] ┌─${Vector.fill(textWidth * 2)("─").mkString}─┐
                 |$text
-                |└─${Vector.fill(newPaper.width)("─").mkString}─┘\n""".stripMargin)
+                |    └─${Vector.fill(textWidth * 2)("─").mkString}─┘""".stripMargin)
 
     "See above"
