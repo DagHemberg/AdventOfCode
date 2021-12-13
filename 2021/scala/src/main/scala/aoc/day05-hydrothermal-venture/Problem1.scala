00f9ee6d3b14dfb4a09433dcb9bbbc135eca12ab
DagHemberg <71427286+DagHemberg@users.noreply.github.com>
2021-12-13 18:56:02 +0100

    compacted

--- 2021/scala/src/main/scala/aoc/day05-hydrothermal-venture/Problem1.scala
+++ 2021/scala/src/main/scala/aoc/day05-hydrothermal-venture/Problem1.scala
@@ -5,16 +5,11 @@ object Problem1 extends Solver("05", 5):
   def name = "Hydrothermal Venture - Part 1"
 
   extension (l: Line) def positions =
-      if l.start.x == l.end.x then l.ys.map(y => Pos(l.start.x, y)).toVector
-      else if l.start.y == l.end.y then l.xs.map(x => Pos(x, l.start.y)).toVector
-      else Vector.empty[Pos]
-
+    if l.start.x == l.end.x then l.ys.map(y => Pos(l.start.x, y)).toVector
+    else if l.start.y == l.end.y then l.xs.map(x => Pos(x, l.start.y)).toVector
+    else Vector.empty[Pos]
   def solve(data: Vector[String]) =
-
-    val lines = data.map {
-        case s"$x1,$y1 -> $x2,$y2" => Line(Pos(x1.toInt, y1.toInt), Pos(x2.toInt, y2.toInt))
-      }
-
+    val lines = data.map { case s"$x1,$y1 -> $x2,$y2" => Line(Pos(x1.toInt, y1.toInt), Pos(x2.toInt, y2.toInt)) }
     val allPositions = lines.flatMap(_.positions)
     val intersections = allPositions
       .groupBy(identity)
