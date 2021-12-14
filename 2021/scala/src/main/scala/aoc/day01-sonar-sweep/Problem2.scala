73eb7fe1be814154db9c0ff88ffa68bafa656871
DagHemberg <71427286+DagHemberg@users.noreply.github.com>
2021-12-14 01:31:37 +0100

    a e s t h e t i c

--- 2021/scala/src/main/scala/aoc/day01-sonar-sweep/Problem2.scala
+++ 2021/scala/src/main/scala/aoc/day01-sonar-sweep/Problem2.scala
@@ -3,10 +3,9 @@ import aoc.utils.*
 
 object Problem2 extends Solver("01", 5):
   def name = "Sonar Sweep - Part 2"
-  def solve(data: Vector[String]) =
-    data
-      .map(_.toInt)
-      .sliding(3)
-      .sliding(2)
-      .filter(x => x.head.sum < x.last.sum)
-      .size
+  def solve(data: Vector[String]) = data
+    .map(_.toInt)
+    .sliding(3)
+    .sliding(2)
+    .filter(x => x.head.sum < x.last.sum)
+    .size
