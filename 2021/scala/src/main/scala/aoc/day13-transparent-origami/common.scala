7127c1bdb2a5a510e1bcc6f1ce8956cd1870ea7d
DagHemberg <71427286+DagHemberg@users.noreply.github.com>
2021-12-14 01:30:33 +0100

    abstracted common elements to common file

--- 2021/scala/src/main/scala/aoc/day13-transparent-origami/common.scala
+++ 2021/scala/src/main/scala/aoc/day13-transparent-origami/common.scala
@@ -1,3 +1,19 @@
 package aoc.day13
+import aoc.utils.*
 
 case class Command(dir: String, value: Int)
+
+def points(using data: Vector[String]) = data
+  .takeWhile(_.nonEmpty)
+  .map { case s"$x,$y" => Pos(x.toInt, y.toInt) }
+
+def commands(using data: Vector[String]) = data
+  .reverse
+  .takeWhile(_.nonEmpty)
+  .reverse
+  .map { case s"fold along $dir=$value" => Command(dir, value.toInt) }
+
+def fold(points: Vector[Pos], commands: Vector[Command]) = 
+  commands.foldLeft(points.toSet)((collectedPoints, command) => command match
+    case Command("x", n) => collectedPoints.map(p => if p.x < n then p else Pos(2 * n - p.x, p.y))
+    case Command("y", n) => collectedPoints.map(p => if p.y < n then p else Pos(p.x, 2 * n - p.y)))
