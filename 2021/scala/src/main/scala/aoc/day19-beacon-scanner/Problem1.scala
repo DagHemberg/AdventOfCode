52db643922531e473020b7fa678b4f2493b6afda
DagHemberg <71427286+DagHemberg@users.noreply.github.com>
2021-12-21 01:34:23 +0100

    init day 19 idk if ill ever finish this one lol

---
+++ 2021/scala/src/main/scala/aoc/day19-beacon-scanner/Problem1.scala
@@ -0,0 +1,7 @@
+package aoc.day19
+import aoc.utils.*
+
+object Problem1 extends Solver("19", 79):
+  def name = "Beacon Scanner - Part 1"
+  def solve(data: Vector[String]) = 
+    1
---
+++ 2021/scala/src/main/scala/aoc/day19-beacon-scanner/Problem2.scala
@@ -0,0 +1,7 @@
+package aoc.day19
+import aoc.utils.*
+
+object Problem2 extends Solver("19", ???):
+  def name = "Beacon Scanner - Part 1"
+  def solve(data: Vector[String]) = 
+    ???
