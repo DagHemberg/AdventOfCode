8dcaf25435a32ffb80db7f6bf04101410b528d7a
DagHemberg <71427286+DagHemberg@users.noreply.github.com>
2022-01-01 18:08:13 +0100

    possible optimization?

--- 2021/src/main/scala/aoc/day13-transparent-origami/Problem2.scala
+++ 2021/src/main/scala/aoc/day13-transparent-origami/Problem2.scala
@@ -19,7 +19,7 @@ object Problem2 extends Solver("13", "See above"):
     val textWidth = (folded.maxBy(_.x).x + 1)
     s"""|┌─${Vector.fill(textWidth * 2)("─").mkString}─┐
         |$text
-    |    └─${Vector.fill(textWidth * 2)("─").mkString}─┘""".stripMargin.log
+    |    └─${Vector.fill(textWidth * 2)("─").mkString}─┘""".stripMargin.log()
 
     // theres no actual checkable expected output for the example 
     // here, so we have to manually make it pass the check
--- 2021/src/main/scala/aoc/day13-transparent-origami/common.scala
+++ 2021/src/main/scala/aoc/day13-transparent-origami/common.scala
@@ -6,6 +6,7 @@ case class Command(dir: String, value: Int)
 def points(using data: Vector[String]) = data
   .takeWhile(_.nonEmpty)
   .map { case s"$x,$y" => Pos(x.toInt, y.toInt) }
+  .toSet
 
 def commands(using data: Vector[String]) = data
   .reverse
@@ -13,7 +14,7 @@ def commands(using data: Vector[String]) = data
   .reverse
   .map { case s"fold along $dir=$value" => Command(dir, value.toInt) }
 
-def fold(points: Vector[Pos], commands: Vector[Command]) = 
-  commands.foldLeft(points.toSet)((collectedPoints, command) => command match
+def fold(points: Set[Pos], commands: Vector[Command]) = 
+  commands.foldLeft(points)((collectedPoints, command) => command match
     case Command("x", n) => collectedPoints.map(p => if p.x < n then p else Pos(2 * n - p.x, p.y))
     case Command("y", n) => collectedPoints.map(p => if p.y < n then p else Pos(p.x, 2 * n - p.y)))
