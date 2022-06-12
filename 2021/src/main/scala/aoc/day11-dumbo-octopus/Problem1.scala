0e92098f547a661b01e3ad015233fa8235246a51
Dag Hemberg <71427286+DagHemberg@users.noreply.github.com>
2022-06-12 14:51:52 +0200

    unnecessary toVector

--- 2021/src/main/scala/aoc/day11-dumbo-octopus/Problem1.scala
+++ 2021/src/main/scala/aoc/day11-dumbo-octopus/Problem1.scala
@@ -4,7 +4,7 @@ import aoc.utils.*
 object Problem1 extends Solver("11", 1656):
   def name = "Dumbo Octopus - Part 1"
   def solve(data: Vector[String]) =
-    var octopodes = data.map(_.split("").toVector.map(nrg => Octopus(nrg.toInt)).toVector).toMatrix
+    var octopodes = data.map(_.split("").toVector.map(nrg => Octopus(nrg.toInt))).toMatrix
     
     var totalBlinks = 0
     for i <- 1 to 100 do
--- 2021/src/main/scala/aoc/day11-dumbo-octopus/Problem2.scala
+++ 2021/src/main/scala/aoc/day11-dumbo-octopus/Problem2.scala
@@ -4,7 +4,7 @@ import aoc.utils.*
 object Problem2 extends Solver("11", 195):
   def name = "Dumbo Octopus - Part 2"
   def solve(data: Vector[String]) = 
-    var octopodes = data.map(_.split("").toVector.map(nrg => Octopus(nrg.toInt)).toVector).toMatrix
+    var octopodes = data.map(_.split("").toVector.map(nrg => Octopus(nrg.toInt))).toMatrix
 
     var found = false
     var step = 1
