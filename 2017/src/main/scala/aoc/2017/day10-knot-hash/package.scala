6220c440e95947a7748113092895d3456a4c3e48
Dag Hemberg <dag.hemberg@gmail.com>
2022-06-16 19:30:04 +0200

    made things a bit more readable

--- 2017/src/main/scala/aoc/2017/day10-knot-hash/package.scala
+++ 2017/src/main/scala/aoc/2017/day10-knot-hash/package.scala
@@ -3,12 +3,16 @@ import aoc.utils.*
 
 package object day10:
   def hash(lengths: List[Int]) = 
-    lengths.foldLeft(((0 until 256).toList, 0, 0)) {
-      case ((list, pos, skip), length) =>
-        val newPos =
-          val ls = (pos until pos + length).map(_ % list.size)
-          (ls zip ls.reverse).toMap withDefault identity
-        (list.indices.map(newPos.apply).map(list.apply).toList,
-        (pos + length + skip) % list.size,
-        skip + 1)
+    lengths.foldLeft(((0 until 256).toList, 0, 0)) { case ((prevList, pos, skip), length) =>
+      val newIndices = 
+        val circled = (pos until pos + length).map(_ % prevList.size)
+        (circled zip circled.reverse).toMap.withDefault(identity)
+
+      val newList = prevList
+        .indices
+        .map(newIndices.apply)
+        .map(prevList.apply)
+        .toList
+
+      (newList, (pos + length + skip) % prevList.size, skip + 1)
     }.head
