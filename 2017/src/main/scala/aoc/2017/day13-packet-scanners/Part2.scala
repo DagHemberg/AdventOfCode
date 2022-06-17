6b578b1350fb635924a449d738ec0d70a6eef626
Dag Hemberg <dag.hemberg@gmail.com>
2022-06-17 21:02:12 +0200

    sped up part 2 :)

--- 2017/src/main/scala/aoc/2017/day13-packet-scanners/Part2.scala
+++ 2017/src/main/scala/aoc/2017/day13-packet-scanners/Part2.scala
@@ -6,7 +6,6 @@ object Part2 extends Problem("2017", "13", "2")(10):
   def solve(data: Seq[String]) = 
     val scanners = parse(data)
 
-    1.doUntil(delay => 
-      def catches(depth: Int, sc: Scanner) = (depth + delay) % (2 * sc.range - 2) == 0
-      !scanners.map(catches).exists(identity)
-    )(_ + 1)
+    1.doUntil
+      (dl => !scanners.exists((dp, sc) => (dp + dl) % (2 * sc.range - 2) == 0))
+      (_ + 1)
