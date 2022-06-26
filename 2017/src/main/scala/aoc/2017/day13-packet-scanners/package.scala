04a80ae70118a597b9de2f76b25dba492aa18df2
Dag Hemberg <dag.hemberg@gmail.com>
2022-06-26 23:15:36 +0200

    pos -> depth, unnecessary toMap

--- 2017/src/main/scala/aoc/2017/day13-packet-scanners/package.scala
+++ 2017/src/main/scala/aoc/2017/day13-packet-scanners/package.scala
@@ -2,9 +2,9 @@ package aoc.y2017
 import aoc.utils.*
 
 package object day13:
-  def parse(data: Seq[String]) = data.map { case s"$pos: $range" =>
-    pos.toInt -> range.toInt
-  }.toMap
+  def parse(data: Seq[String]) = data.map { case s"$depth: $range" =>
+    depth.toInt -> range.toInt
+  }
 
   def catches(depth: Int, range: Int)(using delay: Int = 0) =
     (depth + delay) % (2 * range - 2) == 0
