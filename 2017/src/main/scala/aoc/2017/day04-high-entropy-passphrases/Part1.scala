d34bcbc87662c2e86d46ebe3a685f4bd632780e5
Dag Hemberg <dag.hemberg@gmail.com>
2022-06-26 13:05:47 +0200

    fix bug

--- 2017/src/main/scala/aoc/2017/day04-high-entropy-passphrases/Part1.scala
+++ 2017/src/main/scala/aoc/2017/day04-high-entropy-passphrases/Part1.scala
@@ -1,7 +1,7 @@
 package aoc.y2017.day04
 import aoc.utils.*
 
-object Part1 extends Problem("2017", "04", "1")(2):
+object Part1 extends Problem("2017", "04", "1")(7):
   def name = "High-Entropy Passphrases - Part 1"
   def solve(data: Seq[String]) = 
     data
