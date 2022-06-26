147bfa2cda6bb1e35b728606fa54a091b31dd526
Dag Hemberg <dag.hemberg@gmail.com>
2022-06-26 13:09:12 +0200

    rename

--- 2017/src/main/scala/aoc/2017/day25-the-halting-problem/Part1.scala
+++ 2017/src/main/scala/aoc/2017/day25-the-halting-problem/Part1.scala
@@ -5,14 +5,14 @@ object Part1 extends Problem("2017", "25", "1")(2):
   def name = "The Halting Problem - Part 1"
   def solve(data: Seq[String]) = 
     val (startingState, conditions, steps) = parse(data)
-    val tape = TuringMachine(Set.empty, 0, startingState)
+    val turingMachine = TuringMachine(Set.empty, 0, startingState)
 
-    tape.iterate(t => 
-      conditions(t.state)(t.tape(t.pos).toInt) match
+    turingMachine.iterate(m => 
+      conditions(m.state)(m.tape(m.pos).toInt) match
         case Instruction(write, dir, next) =>
           val tape = write match
-            case 0 => t.tape - t.pos
-            case 1 => t.tape + t.pos
-          TuringMachine(tape, t.pos + dir, next)
+            case 0 => m.tape - m.pos
+            case 1 => m.tape + m.pos
+          TuringMachine(tape, m.pos + dir, next)
       )(steps)
       .tape.size
