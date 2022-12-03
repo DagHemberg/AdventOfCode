c93d4905b4e86ebc931f9fba4e1f8b9372170dd9
Dag Hemberg <dag.hemberg@gmail.com>
2022-12-03 13:52:12 +0100

    revisions

--- 2022/src/main/scala/aoc/2022/03-rucksack-reorganization/package.scala
+++ 2022/src/main/scala/aoc/2022/03-rucksack-reorganization/package.scala
@@ -2,6 +2,7 @@ package aoc.y2022.day03
 import problemutils.*, extensions.*
 
 val priority = 
-  val lower = ('a' to 'z').zipWithIndex.map((c, p) => c -> (p + 1)).toMap
-  val upper = ('A' to 'Z').zipWithIndex.map((c, p) => c -> (p + 27)).toMap
-  lower ++ upper
+  (('a' to 'z') ++ ('A' to 'Z'))
+    .zipWithIndex
+    .toMap
+    .view.mapValues(_ + 1)
