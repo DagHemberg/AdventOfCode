69f1a190a0f9a9b5a0cdce31c95d8d50c9afe949
Dag Hemberg <dag.hemberg@gmail.com>
2022-12-03 12:55:57 +0100

    day 3

---
+++ 2022/src/main/scala/aoc/2022/03-rucksack-reorganization/Part1.scala
@@ -0,0 +1,11 @@
+package aoc.y2022.day03
+import paut.aoc.*
+import problemutils.*, extensions.*
+
+object Part1 extends Problem(3, 2022)(1)(157):
+  def name = "Rucksack Reorganization - Part 1"
+  def solve(data: List[String]) = data
+    .map { rucksack => 
+      val (first, second) = rucksack.splitAt(rucksack.size / 2)
+      priority(first.intersect(second).distinct)
+    }.sum
---
+++ 2022/src/main/scala/aoc/2022/03-rucksack-reorganization/Part2.scala
@@ -0,0 +1,11 @@
+package aoc.y2022.day03
+import paut.aoc.*
+import problemutils.*, extensions.*
+
+object Part2 extends Problem(3, 2022)(2)(70):
+  def name = "Rucksack Reorganization - Part 2"
+  def solve(data: List[String]) = 
+    data
+      .sliding(3, 3)
+      .map(rucksack => priority(rucksack.reduce(_ intersect _).distinct))
+      .sum
