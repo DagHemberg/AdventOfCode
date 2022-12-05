6048af32785175399114d6af05c988825e51942d
Dag Hemberg <dag.hemberg@gmail.com>
2022-12-05 13:38:59 +0100

    fmt

--- 2022/src/main/scala/aoc/2022/05-supply-stacks/Part2.scala
+++ 2022/src/main/scala/aoc/2022/05-supply-stacks/Part2.scala
@@ -4,7 +4,7 @@ import problemutils.*, extensions.*
 
 object Part2 extends Problem(5, 2022)(2)("MCD"):
   def name = "Supply Stacks - Part 2"
-  def solve(data: List[String]) = 
+  def solve(data: List[String]) =
     given Stacks = parseStacks(data)
     given List[Move] = parseInstructions(data)
 
