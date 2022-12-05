e738a73ac45d38a332f1ee79bca4bb1d10a9d73f
Dag Hemberg <dag.hemberg@gmail.com>
2022-12-05 13:38:54 +0100

    move "to" method from helpers to package

--- 2022/src/main/scala/aoc/2022/helpers.scala
+++ 2022/src/main/scala/aoc/2022/helpers.scala
@@ -12,9 +12,6 @@ extension [A](a: A)
 
   def pipe[B](f: A => B) = f(a)
 
-extension (s1: String) 
-  def to(s2: String) = s1.toInt to s2.toInt
-
 extension [A] (xs: IterableOnce[A])
   def sumBy[B](f: A => B)(using num: Numeric[B]) = 
     xs.iterator.foldLeft(num.zero)((sum, x) => sum + f(x))
