16eedf0aca9d7c0cf50a4e85d872bbbdb1e9de69
Dag Hemberg <dag.hemberg@gmail.com>
2022-12-06 10:50:19 +0100

    better variable name

--- 2022/src/main/scala/aoc/2022/06-tuning-trouble/package.scala
+++ 2022/src/main/scala/aoc/2022/06-tuning-trouble/package.scala
@@ -1,5 +1,5 @@
 package aoc.y2022.day06
 import problemutils.*, extensions.*
 
-def findMarker(s: Int)(using buffer: String) = 
-  buffer.sliding(s).indexWhere(str => str == str.distinct) + s
+def findMarker(size: Int)(using buffer: String) = 
+  buffer.sliding(size).indexWhere(str => str == str.distinct) + size
