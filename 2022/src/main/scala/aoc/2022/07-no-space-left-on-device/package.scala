37ad22594bb6532789a25eff910c6f5005ec40d9
Dag Hemberg <dag.hemberg@gmail.com>
2022-12-08 16:09:02 +0100

    minor refactor

--- 2022/src/main/scala/aoc/2022/07-no-space-left-on-device/package.scala
+++ 2022/src/main/scala/aoc/2022/07-no-space-left-on-device/package.scala
@@ -4,15 +4,19 @@ import problemutils.*, extensions.*
 case class Shell(pwd: List[String], folders: Map[List[String], Long])
 val root = Shell(List("/"), Map(List("/") -> 0))
 
-def parse(data: List[String]) = data.foldLeft(root) { (shell, line) => line match
-  case "$ ls"         => shell
-  case "$ cd .."      => shell.copy(pwd = shell.pwd.init)
-  case "$ cd /"       => shell.copy(pwd = List("/"))
-  case s"$$ cd $name" => shell.copy(pwd = shell.pwd :+ name)
-  case s"dir $name"   => shell.copy(folders = shell.folders + ((shell.pwd :+ name) -> 0))
-  case s"$size $name" => 
-    val updated = shell.pwd
-      .inits.toList.reverse.tail
-      .map(path => path -> (shell.folders.getOrElse(path, 0L) + size.toLong))
-    shell.copy(folders = shell.folders ++ updated)
-}.folders
+extension [A] (ls: List[A]) def accumulate = ls.inits.toList.reverse.tail
+
+def parse(data: List[String]) = data
+  .foldLeft(root) { (shell, line) => line match
+    case "$ ls"         => shell
+    case "$ cd .."      => shell.copy(pwd = shell.pwd.init)
+    case "$ cd /"       => shell.copy(pwd = List("/"))
+    case s"$$ cd $name" => shell.copy(pwd = shell.pwd :+ name)
+    case s"dir $name"   => shell
+    case s"$size $name" => 
+      val folders = shell.folders
+      val newFolders = shell.pwd
+        .accumulate
+        .map(path => path -> (folders.getOrElse(path, 0L) + size.toLong))
+      shell.copy(folders = folders ++ newFolders)
+  }.folders
